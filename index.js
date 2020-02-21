77ac77c3d9470e4815364baed619b4c7338561b6
Jonathan East <jonathan_east@ymail.com>
2020-02-21 14:25:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ function sayHiToGrandma(string) {
   if (string === "I love you, Grandma.") {
     return "I love you, too.";
   } else if (string.toUpperCase === string) {
-    return "YES INDEED!";
+    return 'YES INDEED!';
   } else if (string.toLowerCase === string) {
-    return "I can't hear you!";
+    return 'I can\'t hear you!';
   }
 }
